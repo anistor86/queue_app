c8731f480bfbefa4024c37e66b0c0c319b1ff9a9
anistor86 <lucastorani@gmail.com>
2017-10-15 10:33:38 +0100

    Add mysql connection file

---
+++ mysql.inc.php
@@ -0,0 +1,20 @@
+<?php
+  $servername = "your_server_name";
+  $database = "your_database_name";
+  $username = "your_username_here";
+  $password = "your_password_here";
+
+  // Create connection
+
+  $conn = mysqli_connect($servername, $username, $password, $database);
+
+  // Check connection
+
+  if (!$conn) {
+
+      die("Connection failed: " . mysqli_connect_error());
+
+  }
+  //echo "Connected successfully";
+
+?>
