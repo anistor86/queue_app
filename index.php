dc3c8d29af37c64b794262d26b91f832988f8253
anistor86 <lucastorani@gmail.com>
2017-10-15 10:32:23 +0100

    Add Index and app core

---
+++ index.php
@@ -0,0 +1,139 @@
+<?php include("mysql.inc.php"); ?>
+<!doctype html>
+<html>
+<head>
+	<title>Council Queue App</title>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css"> <!-- load bootstrap via CDN -->
+	<link rel="stylesheet" href="style.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script> <!-- load jquery via CDN -->
+	<script src="script.js"></script> <!-- load our javascript file -->
+</head>
+<body>
+	<div class="container">
+		<h1>Council Queue App</h1>
+		<div class="col-sm-6">
+			<div class="panel panel-default">
+        <div class="panel-heading col-sm-12">
+            <h3 class="panel-title">New Customer</h3>
+        </div>
+				<div class="panel-body" id="newCustomer">
+					<div class="row">
+						<!-- OUR FORM -->
+						<form action="process.php" method="POST">
+							<!-- SERVICES -->
+							<div id="services-group" class="form-group col-sm-12 lista">
+								<label for="services">Services</label>
+								<div class="radio radio-primary">
+									<input type="radio" name="servicesGroup" id="housing" value="Housing">
+									<label for="housing">Housing</label>
+								</div>
+								<div class="radio radio-primary">
+									<input type="radio" name="servicesGroup" id="benefits" value="Benefits">
+									<label for="benefits">Benefits</label>
+								</div>
+								<div class="radio radio-primary">
+									<input type="radio" name="servicesGroup" id="council-tax" value="Council Tax">
+									<label for="council-tax">Council Tax</label>
+								</div>
+								<div class="radio radio-primary">
+									<input type="radio" name="servicesGroup" id="fly-tipping" value="Fly-tipping">
+									<label for="fly-tipping">Fly-tipping</label>
+								</div>
+								<div class="radio radio-primary">
+									<input type="radio" name="servicesGroup" id="missed-bin" value="Missed Bin">
+									<label for="missed-bin">Missed Bin</label>
+								</div>
+								<!-- errors will go here -->
+							</div>
+							<!-- TYPE -->
+							<div id="type-group" class="form-group col-sm-12 lista">
+								<label for="type">Type</label>
+								<div class="input-group">
+    							<div id="radioBtn" class="btn-group">
+    								<a class="btn btn-primary btn-sm notActive" data-toggle="typeGroup" data-title="Citizen">Citizen</a>
+                    <a class="btn btn-primary btn-sm notActive" data-toggle="typeGroup" data-title="Organisation">Organisation</a>
+    								<a class="btn btn-primary btn-sm notActive" data-toggle="typeGroup" data-title="Anonymous">Anonymous</a>
+    						</div>
+    						<input type="hidden" name="typeGroup" id="typeGroup">
+    					</div>
+
+
+								<!-- errors will go here -->
+							</div>
+							<!-- TITLE -->
+							<div id="title-group" class="form-group col-sm-12 lista">
+							  <label for="titleGroup">Title</label>
+							    <select id="selectTitleGroup" name="titleGroup" class="form-control">
+							      <option value="Mr.">Mr.</option>
+							      <option value="Mrs.">Mrs.</option>
+							      <option value="Ms.">Ms.</option>
+										<option value="Miss">Miss</option>
+							    </select>
+							<!-- errors will go here -->
+							</div>
+							<!-- FIRST NAME -->
+							<div id="firstname-field" class="form-group col-sm-12 lista">
+								<label for="firstname">First Name</label>
+								<input type="text" class="form-control" name="firstname" placeholder="First Name">
+								<!-- errors will go here -->
+							</div>
+							<!-- LAST NAME -->
+							<div id="lastname-field" class="form-group col-sm-12 lista">
+								<label for="lastname">Last Name</label>
+								<input type="text" class="form-control" name="lastname" placeholder="Last Name">
+								<!-- errors will go here -->
+							</div>
+							<div class="form-group col-sm-12 lista">
+								<button type="submit" class="btn btn-info">Submit <span class="fa fa-arrow-right"></span></button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="panel panel-default">
+          <div class="panel-heading col-sm-12">
+              <h3 class="panel-title">Queue</h3>
+          </div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-sm-12" id="queue">
+								<div class="row head">
+									<div class="col-xs-1 col-sm-1">#</div>
+									<div class="col-xs-2 col-sm-2">Type</div>
+									<div class="col-xs-4 col-sm-4">Name</div>
+									<div class="col-xs-3 col-sm-3">Service</div>
+									<div class="col-xs-1 col-sm-1">Time</div>
+								</div>
+							<!-- retrieve data from db -->	
+							<?php
+								$sql = "SELECT * FROM queue_app";
+
+								$result = mysqli_query($conn, $sql);
+
+								if (mysqli_num_rows($result) > 0) {
+								    // output data of each row
+										$count = 1;
+								    while($row = mysqli_fetch_assoc($result)) {
+											if($row["type"] == "Anonymous" || $row["type"] == "Organisation"){
+												$row["title"] = "";
+												$row["last_name"] = "";
+											}
+											echo "<div class=\"row lista\"><div class=\"col-xs-1 col-sm-1\">" . $count . "</div><div class=\"col-xs-2 col-sm-2\">" . $row["type"] .
+											"</div><div class=\"col-xs-4 col-sm-4\">" . $row["title"] . " " . $row["first_name"] . " " . $row["last_name"] .
+											"</div><div class=\"col-xs-3 col-sm-3\">" .	$row["services"] . "</div><div class=\"col-xs-1 col-sm-1\">" .
+											substr($row["time_reg"], 0, 5) . "</div></div>";
+											$count++;
+										}
+								}
+								mysqli_close($conn);
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,69 @@
+<?php
+
+
+$errors         = array();  	// array to hold validation errors
+$data 			= array(); 		// array to pass back data
+// validate the variables ======================================================
+	// if any of these variables don't exist, add an error to our $errors array
+	if (empty($_POST['services']))
+		$errors['services'] = 'Service is required.';
+	if (empty($_POST['type']))
+		$errors['type'] = 'Choose a type, please!.';
+	if (empty($_POST['titleGroup']))
+		$errors['titleGroup'] = 'Choose a title, please!';
+	if (empty($_POST['firstname']))
+		$errors['firstname'] = 'Choose a first name, please!';
+	if (empty($_POST['lastname']))
+		$errors['lastname'] = 'Choose a last name, please!';
+// return a response ===========================================================
+	// if there are any errors in our errors array, return a success boolean of false
+	if ( ! empty($errors)) {
+		// if there are items in our errors array, return those errors
+		$data['success'] = false;
+		$data['errors']  = $errors;
+	} else {
+
+		// if there are no errors process our form, then return a message
+		// DO ALL YOUR FORM PROCESSING HERE
+		// THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
+		// show a message of success and provide a true success variable
+
+
+
+		include("mysql.inc.php");
+
+		$services = $_POST['services'];
+		$type = $_POST['type'];
+		$title_group = trim($_POST['titleGroup']);
+		$first_name = mysqli_real_escape_string($conn, trim($_POST['firstname']));
+		$last_name = mysqli_real_escape_string($conn, trim($_POST['lastname']));
+		$time_reg = date('H:i:s');
+
+		$sql = "INSERT INTO queue_app (services, type, title, first_name, last_name, time_reg) VALUES ('$services', '$type', '$title_group', '$first_name' ,'$last_name', '$time_reg')";
+
+		if (mysqli_query($conn, $sql)) {
+				$result = mysqli_query($conn, "SELECT * FROM queue_app");
+				$count = mysqli_num_rows($result);
+
+				$data['success'] = true;
+				$data['message'] = 'Success!';
+				$data['count'] = $count;
+				$data['services'] = $services;
+				$data['type'] = $type;
+				$data['title_group'] = $title_group;
+				$data['first_name'] = stripslashes($first_name);
+				$data['last_name'] = stripslashes($last_name);
+				$data['time_reg'] = $time_reg;
+		} else {
+				$errors['database'] = 'Db Error.';
+				$data['dbInsert'] = 'Fail!';
+				$data['success'] = false;
+				$data['errors']  = $errors;
+		}
+		mysqli_close($conn);
+	}
+	// return all our data to an AJAX call
+
+	echo json_encode($data);
+
+	?>
